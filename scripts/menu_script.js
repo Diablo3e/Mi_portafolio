4dd85d21766d4ab2c4225607a4d6d20e71077ca6
AlanDGB <hpro3961@gmail.com>
2026-01-31 21:47:55 -0600

    borrado de div de sobra, cambio de stylos y añadir bold y size

--- scripts/menu_script.js
+++ scripts/menu_script.js
@@ -1,11 +1,20 @@
+//accion del boton del menu
 function toggleMenu() {
   const sidebar = document.getElementById("mySidebar");
+  const content = document.getElementById("mainContent");
+
   if (sidebar.style.width === "200px") {
     sidebar.style.width = "0";
+    content.style.marginLeft = "0";
   } else {
     sidebar.style.width = "200px";
+    content.style.marginLeft = "200px";
   }
 }
+
+
+//Burbuja de fondo
+
 tsParticles.load("tsparticles", {
   particles: {
     number: { value: 50 },
